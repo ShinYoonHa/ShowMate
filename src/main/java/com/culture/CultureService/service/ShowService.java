9eae11dcdfc4771ed58b7551f66b0a5d3ecb3f0b
ShinYoonHa <hello.git.idc06012@naver.com>
2024-07-08 19:29:50 +0900

    feat: add Service. db에서 공연정보를 불러오는 서비스 추가.

---
+++ src/main/java/com/culture/CultureService/service/ShowService.java
@@ -0,0 +1,22 @@
+package com.culture.CultureService.service;
+
+import com.culture.CultureService.dto.ShowSearchDto;
+import com.culture.CultureService.entity.ShowEntity;
+import com.culture.CultureService.repository.ShowRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor  //final, @NonNull 붙은 변수 자동 주입
+public class ShowService {
+    private final ShowRepository showRepository; //자동 주입
+
+    //공연 목록 페이지 조회
+    @Transactional(readOnly = true) //조회용 쿼리임을 명시
+    public Page<ShowEntity> getShowListPage(ShowSearchDto showSearchDto, Pageable pageable) {
+        return showRepository.getShowListPage(showSearchDto, pageable);
+    }
+}
