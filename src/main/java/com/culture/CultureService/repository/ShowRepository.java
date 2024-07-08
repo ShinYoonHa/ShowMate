38236053a77122441627839102195904707abc87
ShinYoonHa <hello.git.idc06012@naver.com>
2024-07-08 19:31:31 +0900

    refactor: delete @Repository. JPA 레포지토리 상속 시 어노테이션 불필요

--- src/main/java/com/culture/CultureService/repository/ShowRepository.java
+++ src/main/java/com/culture/CultureService/repository/ShowRepository.java
@@ -2,10 +2,8 @@ package com.culture.CultureService.repository;
 
 import com.culture.CultureService.entity.ShowEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ShowRepository extends JpaRepository<ShowEntity, Long> {
+public interface ShowRepository extends JpaRepository<ShowEntity, Long>, ShowRepositoryCustom {
     ShowEntity findByShowId(String showId);
-    boolean existsByShowId(String showId);
+    boolean existsByShowId(String ShowId);
 }
