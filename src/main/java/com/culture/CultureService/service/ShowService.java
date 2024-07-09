e21b0e037d2245dce68905d07f3e0ac9c3ea5dfc
ShinYoonHa <hello.git.idc06012@naver.com>
2024-07-09 17:03:25 +0900

    refactor: update 공연상세보기 위한 Service 작성.

--- src/main/java/com/culture/CultureService/service/ShowService.java
+++ src/main/java/com/culture/CultureService/service/ShowService.java
@@ -1,8 +1,10 @@
 package com.culture.CultureService.service;
 
+import com.culture.CultureService.dto.ShowDto;
 import com.culture.CultureService.dto.ShowSearchDto;
 import com.culture.CultureService.entity.ShowEntity;
 import com.culture.CultureService.repository.ShowRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor  //final, @NonNull 붙은 변수 자동 주입
 public class ShowService {
     private final ShowRepository showRepository; //자동 주입
@@ -18,5 +21,10 @@ public class ShowService {
     @Transactional(readOnly = true) //조회용 쿼리임을 명시
     public Page<ShowEntity> getShowListPage(ShowSearchDto showSearchDto, Pageable pageable) {
         return showRepository.getShowListPage(showSearchDto, pageable);
+    }
+    public ShowDto getShowDetail(Long id) {
+        ShowEntity showEntity = showRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        //ShowEntity -> ShowDto
+        return ShowDto.of(showEntity);
     }
 }
