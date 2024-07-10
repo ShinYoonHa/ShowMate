83460c1fee81a397b4c3e7a262a03c75c35ba28f
tjsdnwkdp <0811csw@naver.com>
2024-07-10 17:36:47 +0900

    refactor:[add] AwardRepositoryCustom 인터페이스의 구현 클래스인 AwardRepositoryCustomImpl.java 생성

---
+++ src/main/java/com/culture/CultureService/repository/AwardRepositoryCustomImpl.java
@@ -0,0 +1,69 @@
+package com.culture.CultureService.repository;
+
+import com.culture.CultureService.dto.AwardSearchDto;
+import com.culture.CultureService.entity.AwardEntity;
+import com.culture.CultureService.entity.QAwardEntity;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.thymeleaf.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class AwardRepositoryCustomImpl implements AwardRepositoryCustom{
+    private final JPAQueryFactory queryFactory;
+
+    @Autowired
+    public AwardRepositoryCustomImpl(EntityManager em){
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+    private BooleanExpression searchGenreEq(String genre) {
+        return StringUtils.isEmpty(genre) ? null : QAwardEntity.awardEntity.genre.eq(genre);
+    }
+    private BooleanExpression searchByEq(String searchBy, String searchQuery) {
+        if (StringUtils.isEmpty(searchQuery)) {
+            return null;
+        }
+        if ("title".equals(searchBy)) {
+            return QAwardEntity.awardEntity.title.containsIgnoreCase(searchQuery);
+        } else if ("awardName".equals(searchBy)) {
+            return QAwardEntity.awardEntity.awardId.containsIgnoreCase(searchQuery); // 수정된 부분
+        }
+        return null;
+    }
+
+
+
+    @Override
+    public Page<AwardEntity> getAwardListPage(AwardSearchDto awardSearchDto, Pageable pageable) {
+        QAwardEntity award = QAwardEntity.awardEntity;
+
+        QueryResults<AwardEntity> results = queryFactory
+                .selectFrom(award)
+                .where(
+                        searchGenreEq(awardSearchDto.getSearchGenre()),
+                        searchByEq(awardSearchDto.getSearchBy(), awardSearchDto.getSearchQuery())
+                )
+                .orderBy(award.Id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<AwardEntity> awards = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(awards, pageable, total);
+    }
+
+
+}
