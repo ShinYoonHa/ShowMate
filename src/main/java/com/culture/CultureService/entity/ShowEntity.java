16921b27b75faa4d25e0c8eefcde2228ef65a316
ShinYoonHa <hello.git.idc06012@naver.com>
2024-07-11 12:01:15 +0900

    refactor: ShowEntity에 PlaceDto랑 연관관계 설정. PlaceName 변수 삭제

--- src/main/java/com/culture/CultureService/entity/ShowEntity.java
+++ src/main/java/com/culture/CultureService/entity/ShowEntity.java
@@ -1,17 +1,13 @@
 package com.culture.CultureService.entity;
 
-import com.culture.CultureService.dto.ShowDto;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table
-@NoArgsConstructor  // 기본 생성자 자동 생성
-@AllArgsConstructor
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class ShowEntity {
 
     @Id
@@ -23,19 +19,21 @@ public class ShowEntity {
     private String stDate; //공연 시작일
     private String edDate; //공연 종료일
 
-    private String placeName; //공연 시설명
     private String cast; //출연진
     private String staff; //제작진
     private String runtime; //런타임
     private String age; //관람연령
     private String producer; //제작사 (기획제작자)
+    @Column(length = 500)
     private String ticketPrice; //티켓 가격
-    private String posterUrl; //포스터 이미지 경로 http://www...
+    private String posterUrl; //포스터 이미지 경로
 
     private String genre; //장르
     private String state; //공연상태
     private String storyUrl; //소개 이미지
-    private String placeId; //공연 시설 아이디
     private String time; //공연 시간
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "place_id", referencedColumnName = "placeId")
+    private PlaceEntity place;
 }
