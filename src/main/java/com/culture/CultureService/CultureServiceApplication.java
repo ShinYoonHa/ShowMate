d3ffd0a3f5c12760f4afa62948e9924f18e88c09
ShinYoonHa <hello.git.idc06012@naver.com>
2024-07-11 12:14:41 +0900

    refactor: 스프링 실행환경에 테스트 메소드 추가

--- src/main/java/com/culture/CultureService/CultureServiceApplication.java
+++ src/main/java/com/culture/CultureService/CultureServiceApplication.java
@@ -1,5 +1,6 @@
 package com.culture.CultureService;
 
+import com.culture.CultureService.service.PlaceApiService;
 import com.culture.CultureService.service.ShowApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +12,8 @@ public class CultureServiceApplication implements CommandLineRunner {
 
 	@Autowired
 	private ShowApiService showApiService;
+	@Autowired
+	private PlaceApiService placeApiService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CultureServiceApplication.class, args);
@@ -19,11 +22,12 @@ public class CultureServiceApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// 임의의 값으로 테스트
-//		String stDate = "20240101";
-//		String edDate = "20240730";
-//		String page = "1";
-//		String rows = "100";
-//
-//		showApiService.fetchAndSaveShowData(stDate, edDate, page, rows);
+		//String stDate = "20240101";
+		//String edDate = "20240730";
+		//String page = "1";
+		//String rows = "100";
+
+		//showApiService.fetchAndSaveShowData(stDate, edDate, page, rows);
+		//placeApiService.fetchAndSaveAwardData(stDate, edDate, page, rows);
 	}
 }
