b7f68a57ac8fb462bc90a54bf3b55984afb6de1d
ShinYoonHa <hello.git.idc06012@naver.com>
2024-07-11 12:00:14 +0900

    refactor: ShowDto mapping 후 placeDto도 함께 저장되도록 수정.

--- src/main/java/com/culture/CultureService/dto/ShowDto.java
+++ src/main/java/com/culture/CultureService/dto/ShowDto.java
@@ -1,18 +1,13 @@
 package com.culture.CultureService.dto;
 
 import com.culture.CultureService.entity.ShowEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.modelmapper.ModelMapper;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class ShowDto {
-
     private Long id; //db 저장 시 아이디
 
     private String showId; //공연 아이디
@@ -20,7 +15,6 @@ public class ShowDto {
     private String stDate; // 공연 시작일
     private String edDate; // 공연 종료일
 
-    private String placeName; // 시설명
     private String cast; // 출연진
     private String staff; // 제작진
     private String runtime; // 런타임
@@ -32,23 +26,25 @@ public class ShowDto {
     private String genre; // 장르
     private String state; // 공연 상태
     private String storyUrl; //소개 이미지
-    private String placeId; //공연 시설 아이디
     private String time; //공연 시간
 
+    private PlaceDto placeDto; //공연시설 Dto
+
     public ShowDto(String showId, String title, String stDate, String edDate,
-                   String placeName, String genre, String posterUrl) {
+                   String genre, String posterUrl) {
         this.showId = showId;
         this.title = title;
         this.stDate = stDate;
         this.edDate = edDate;
-        this.placeName = placeName;
         this.genre = genre;
         this.posterUrl = posterUrl;
     }
-    //Model Mapper
     private static ModelMapper modelMapper = new ModelMapper();
 
     public static ShowDto of(ShowEntity showEntity) { //ShowEntity 정보를 ShowDto에 띄워주는 메소드
-        return modelMapper.map(showEntity, ShowDto.class);
+       ShowDto showDto = modelMapper.map(showEntity, ShowDto.class);
+       showDto.setPlaceDto(PlaceDto.of(showEntity.getPlace()));
+
+       return showDto;
     }
 }
