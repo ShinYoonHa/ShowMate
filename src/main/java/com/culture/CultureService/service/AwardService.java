30fc996fe092079dd44e7e9cdd00a5363f4c7540
tjsdnwkdp <0811csw@naver.com>
2024-07-12 17:58:19 +0900

    refactor:[update]AwardService 에 모델 데이터추가

--- src/main/java/com/culture/CultureService/service/AwardService.java
+++ src/main/java/com/culture/CultureService/service/AwardService.java
@@ -1,16 +1,15 @@
 package com.culture.CultureService.service;
 
 import com.culture.CultureService.dto.AwardSearchDto;
-import com.culture.CultureService.dto.ShowSearchDto;
 import com.culture.CultureService.entity.AwardEntity;
-import com.culture.CultureService.entity.ShowEntity;
 import com.culture.CultureService.repository.AwardRepository;
-import com.culture.CultureService.repository.ShowRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor  //final, @NonNull 붙은 변수 자동 주입
@@ -21,5 +20,11 @@ public class AwardService {
     @Transactional(readOnly = true) //조회용 쿼리임을 명시
     public Page<AwardEntity> getAwardListPage(AwardSearchDto awardSearchDto, Pageable pageable) {
         return awardRepository.getAwardListPage(awardSearchDto, pageable);
+    }
+
+    // 모든 수상작 목록 조회
+    @Transactional(readOnly = true)
+    public List<AwardEntity> getAllAwards() {
+        return awardRepository.findAll();
     }
 }
