3f1e2eb82176bac386340288befb7262a15304f5
ShinYoonHa <hello.git.idc06012@naver.com>
2024-07-12 16:25:02 +0900

    refactor: 공연장상세정보를 위한 PlaceService 내 메소드 추가

--- src/main/java/com/culture/CultureService/service/PlaceService.java
+++ src/main/java/com/culture/CultureService/service/PlaceService.java
@@ -1,8 +1,10 @@
 package com.culture.CultureService.service;
 
+import com.culture.CultureService.dto.PlaceDto;
 import com.culture.CultureService.dto.PlaceSearchDto;
 import com.culture.CultureService.entity.PlaceEntity;
 import com.culture.CultureService.repository.PlaceRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class PlaceService {
     private final PlaceRepository placeRepository;
@@ -18,6 +21,14 @@ public class PlaceService {
     @Transactional(readOnly = true) //조회용 쿼리임을 명시
     public Page<PlaceEntity> getPlaceListPage(PlaceSearchDto placeSearchDto, Pageable pageable) {
         return placeRepository.getPlaceListPage(placeSearchDto, pageable);
+    }
+    //공연장정보 페이지를 위한 PlaceDto 반환
+    public PlaceDto getPlaceDetail(Long id) {
+        PlaceEntity placeEntity = placeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당 ID를 가진 시설이 존재하지 않습니다."));
+        // PlaceEntity를 PlaceDto로 변환
+        PlaceDto placeDto = PlaceDto.of(placeEntity);
+        return placeDto;
     }
 
 }
