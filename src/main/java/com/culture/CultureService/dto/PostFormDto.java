0258b71e1c4885fd3c398c3a4c475e3d71893331
tjsdnwkdp <0811csw@naver.com>
2024-07-16 10:56:06 +0900

    refactor:[update]postFormDto 공연정보추가

--- src/main/java/com/culture/CultureService/dto/PostFormDto.java
+++ src/main/java/com/culture/CultureService/dto/PostFormDto.java
@@ -5,14 +5,44 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class PostFormDto {
+    private Long id; //
     private String title; //제목
     private String content; //내용
     private String author; //작성자
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate postDate; //새로운 날짜 피트
+
+    private LocalDateTime regTime; // 작성시간 추가
+
+
+    // 공연 정보 추가
+    private String showId;
+    private String showTitle;
+    private String showPeriod;
+    private String showGenre;
+    private String showPosterUrl;
+
+    @Override
+    public String toString() {
+        return "PostFormDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                ", postDate=" + postDate +
+                ", regTime=" + regTime +
+                ", showId='" + showId + '\'' +
+                ", showTitle='" + showTitle + '\'' +
+                ", showPeriod='" + showPeriod + '\'' +
+                ", showGenre='" + showGenre + '\'' +
+                ", showPosterUrl='" + showPosterUrl + '\'' +
+                '}';
+    }
+
 }
