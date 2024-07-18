8b3a6fdc5cd1df2a3761e9940f747aa9ff562523
tjsdnwkdp <0811csw@naver.com>
2024-07-18 17:21:54 +0900

    refactor:[update]PostService 게시물에 신청('applyPost'), 신청취소('cancelApplyPost') 메서드 추가

--- src/main/java/com/culture/CultureService/service/PostService.java
+++ src/main/java/com/culture/CultureService/service/PostService.java
@@ -1,7 +1,9 @@
 package com.culture.CultureService.service;
 
 import com.culture.CultureService.dto.PostFormDto;
+import com.culture.CultureService.entity.Member;
 import com.culture.CultureService.entity.PostEntity;
+import com.culture.CultureService.repository.MemberRepository;
 import com.culture.CultureService.repository.PostRepository;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
@@ -23,6 +25,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final MemberRepository memberRepository;
 
 
     public List<PostEntity> getPosts(){
@@ -130,6 +133,66 @@ public class PostService {
         return null;
     }
 
+    public String applyPost(Long id, String username) {
+        // ID로 게시물을 찾고, 없을 경우 EntityNotFoundException 을 발생
+        PostEntity postEntity = postRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        // 게시물의 작성자 이메일과 현재 사용자의 이메일 가져옴
+        String authorEmail = postEntity.getAuthor();
+        String email = getCurrentUserEmail();
+        // 현재 사용자의 이메일로 회원 정보를 조회
+        Member member = memberRepository.findByEmail(email);
+        // 자신이 작성한 게시물에 신청하는 경우 'self' 반환
+        if (authorEmail.equals(email)) {
+            return "self";
+        }
+        // 게사물의 현재 인원이 최대 인원을 초과하는 경우 'max'를 반환
+        if (postEntity.getCurrentPeople() >= postEntity.getMaxPeople()) {
+            return "max"; // 최대 정원 초과
+        }
+        // 현재 인원 수를 증가시키고, 회원을 게시물의 멤버 목록에 추가
+        postEntity.setCurrentPeople(postEntity.getCurrentPeople() + 1);
+        postEntity.getMembers().add(member);
+        // 변경된 게시물 정보 저장
+        postRepository.save(postEntity);
+        return "success";
+    }
 
+    public String cancelApplyPost(Long id, String username) {
+        //ID로 게시물 찾고, 없을 경우 EntityNotFoundException 발생
+        PostEntity postEntity = postRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        // 자신이 작성한 게시물의 신청을 취소하는 경우 'self' 반환
+        if (postEntity.getAuthor().equals(username)) {
+            return "self"; // 자신이 작성한 게시물
+        }
+        // 게시물의 현재 인원이 0 이하인 경우 'min' 반환
+        if (postEntity.getCurrentPeople() <= 0) {
+            return "min"; // 최소 인원 미만
+        }
+        // 현재 인원 수를 감소, 해당 회원을 게시물의 멤버 목록에서 제거
+        postEntity.setCurrentPeople(postEntity.getCurrentPeople() - 1);
+        Member member = memberRepository.findByEmail(username);
+        // 해당 회원을 게시물의 멤버 목록에서 찾아 제거
+        // 게시물에 등록된 회원 목록 가져옴
+        List<Member> lists = postEntity.getMembers();
+        // 찾는 회원의 인덱스 초기화 회원을 찾기 못할 경우를 대비해-1로 설정
+        int index = -1;
+        // 회원 목록을 돌면서 특정 회원 ID와 목록 내의 회원 ID가 일치하는지 확인
+        for(int i =0;i<lists.size();i++){
+            if(member.getId() == lists.get(i).getId()){
+                index = i; // 일치하는 회원의 인덱스 저장
+                break;// 일치하는 회원을 찾은 후 종료
+            }
+        }
+        // index가 -1이 아니면, 유요한 인덱스 찾은것
+        if(index != -1){
+            // 유요한 인덱스가 존재하므로, list에서 해당 인덱스의 Member 객체 제거
+            lists.remove(index);
+        }
+        // 변경된 게시물 정보를 저장
+        postRepository.save(postEntity);
+        return "success";
+    }
 
 }
