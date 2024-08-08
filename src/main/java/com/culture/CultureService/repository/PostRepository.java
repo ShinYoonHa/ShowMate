74a6c31ad09aac1e5e0934f5bdbb8375e4435b5e
tjsdnwkdp <hello.git.0811csw@naver.com>
2024-08-08 18:51:59 +0900

    [update] PostRepository.java 조회수 증가 메서드 추가

--- src/main/java/com/culture/CultureService/repository/PostRepository.java
+++ src/main/java/com/culture/CultureService/repository/PostRepository.java
@@ -4,7 +4,9 @@ import com.culture.CultureService.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,4 +22,9 @@ public interface PostRepository extends JpaRepository<PostEntity, Long> {
     Page<PostEntity> findByKeyword(String keyword, Pageable pageable);
     List<PostEntity> findAllByOrderByRegTimeDesc();
     Page<PostEntity> findAllByOrderByRegTimeDesc(Pageable pageable);
+    //조회수 증가 메서드를 추가
+    @Modifying
+    @Query("UPDATE PostEntity p SET p.viewCount = p.viewCount + 1 WHERE p.id = :postId")
+    void incrementViewCount(@Param("postId") Long postId);
+
 }
